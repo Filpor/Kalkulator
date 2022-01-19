5da76f35218490285074dc0768df52c85ddfc0bd
Filpor <filip.porebinski@o2.pl>
2022-01-19 19:13:21 +0100

    Everthing_looks_ok

--- kalkulator1.py
+++ kalkulator1.py
@@ -3,21 +3,21 @@ import math
 logging.basicConfig(level=logging.DEBUG)
 def kalkulator(first,second,third):
     if first==1:
-        logging.info("Dodaję %d i %d" %(second,third))
+        logging.info("Dodaję %f i %f" %(second,third))
         result=second+third
     elif first==2:
-        logging.info("Odajemuję %d i %d" %(second,third))
+        logging.info("Odajemuję %f i %f" %(second,third))
         result=second-third
     elif first==3:
-        logging.info("Mnoze %d i %d" %(second,third))
+        logging.info("Mnoze %f i %f" %(second,third))
         result=second*third
     elif first==4:
-        logging.info("Dziele %d i %d" %(second,third))
+        logging.info("Dziele %f i %f" %(second,third))
         result=second/third
     else:
         logging.error("Niepoprawne określenie działania")
         exit(1)
-    logging.info("Wynik: %d" % result)
+    logging.info("Wynik: %f" % result)
 if __name__ =='__main__':
     print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
     first=int(input())
